4250e3ded6ba70b4bd7df62fa6d4453d66520688
RaulS963 <35034505+RaulS963@users.noreply.github.com>
2018-03-17 02:32:35 +0530

    Update urls.py

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path , include
 from . import views
+from django.views.generic import ListView , DetailView
+from blog.models import Post
 
 urlpatterns = [
     path('', views.main , name='main' ),
 	path('personal/', views.personal , name='personal' ),
 	path('about/', views.about , name='about' ),
+	path('blog-list/', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="list.html")),
 	
 ]
